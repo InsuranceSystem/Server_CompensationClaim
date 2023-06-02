85df1e7acad2cb31b7adc57d0435a0fd50ff88bb
suyeon <ibaesuyeon@Naver.com>
2023-06-03 04:51:19 +0900

    [Fix] DAO수정
    CarAccident, CompensationClaim, Survey DAO에서 SQL수정

--- src/Dao/CompensationClaimDao.java
+++ src/Dao/CompensationClaimDao.java
@@ -15,32 +15,43 @@ public class CompensationClaimDao extends Dao{
 		}
 	}
 	public void create(CompensationClaim compensationClaim) throws Exception {
-		String query = "insert into CompensationClaim(CCID, insuranceID, customerID, receptionistName, receptionistPNumber, relationshipOfContractor, documentFilePath, bank, accountNumber, accountHolderName) values ('"+
-				compensationClaim.getCCID() + "','" + compensationClaim.getInsuranceID() + "','" + compensationClaim.getCustomerID() + "','" + compensationClaim.getReceptionistName() + "','"
-				+ compensationClaim.getReceptionistPNumber() + "','" + compensationClaim.getRelationshipOfContractor() + "','" + compensationClaim.getDocumentFilePath() + "','" + compensationClaim.getBank() + "','"
-				+ compensationClaim.getAccountNumber() + "','" + compensationClaim.getAccountHolderName() + ");";
-		System.out.println(query);
+		String query = "INSERT INTO CompensationClaim (CCID, insuranceID, customerID, receptionistName, receptionistPNumber, relationshipOfContractor, documentFilePath, bank, accountNumber, accountHolderName) VALUES ('" +
+				compensationClaim.getCCID() + "','" +
+				compensationClaim.getInsuranceID() + "','" +
+				compensationClaim.getCustomerID() + "','" +
+				compensationClaim.getReceptionistName() + "','" +
+				compensationClaim.getReceptionistPNumber() + "','" +
+				compensationClaim.getRelationshipOfContractor() + "','" +
+				compensationClaim.getDocumentFilePath() + "','" +
+				compensationClaim.getBank() + "','" +
+				compensationClaim.getAccountNumber() + "','" +
+				compensationClaim.getAccountHolderName() + "');";
 		super.create(query);
 	}
 	public void createAll(ArrayList<CompensationClaim> compensationClaimList) throws Exception {
 		CompensationClaim compensationClaim;
 		for(int i=0;i<=compensationClaimList.size();i++) {
 			compensationClaim = compensationClaimList.get(i);
-			String query = "insert into CompensationClaim(CCID, insuranceID, customerID, receptionistName, receptionistPNumber, relationshipOfContractor, documentFilePath, bank, accountNumber, accountHolderName) values ('"+
-					compensationClaim.getCCID() + "','" + compensationClaim.getInsuranceID() + "','" + compensationClaim.getCustomerID() + "','" + compensationClaim.getReceptionistName() + "','"
-					+ compensationClaim.getReceptionistPNumber() + "','" + compensationClaim.getRelationshipOfContractor() + "','" + compensationClaim.getDocumentFilePath() + "'," + compensationClaim.getBank() + ",'"
-					+ compensationClaim.getAccountNumber() + "'," + compensationClaim.getAccountHolderName() + ");";
-			System.out.println(query);
-		super.create(query);
+			String query = "INSERT INTO CompensationClaim (CCID, insuranceID, customerID, receptionistName, receptionistPNumber, relationshipOfContractor, documentFilePath, bank, accountNumber, accountHolderName) VALUES ('" +
+					compensationClaim.getCCID() + "','" +
+					compensationClaim.getInsuranceID() + "','" +
+					compensationClaim.getCustomerID() + "','" +
+					compensationClaim.getReceptionistName() + "','" +
+					compensationClaim.getReceptionistPNumber() + "','" +
+					compensationClaim.getRelationshipOfContractor() + "','" +
+					compensationClaim.getDocumentFilePath() + "','" +
+					compensationClaim.getBank() + "','" +
+					compensationClaim.getAccountNumber() + "','" +
+					compensationClaim.getAccountHolderName() + "');";
+			super.create(query);
 		}
 	}
 	public ArrayList<CompensationClaim> retrieveAll() throws Exception {
 		String query = "select * from CompensationClaim;";
-		System.out.println(query);
 		ResultSet results = super.retrieve(query);
-        ArrayList<CompensationClaim> compensationClaimList = new ArrayList<CompensationClaim>();
+		ArrayList<CompensationClaim> compensationClaimList = new ArrayList<CompensationClaim>();
 		CompensationClaim compensationClaim;
-        while (results.next()){
+		while (results.next()){
 			compensationClaim = new CompensationClaim();
 			compensationClaim.setCCID(results.getString("CCID"));
 			compensationClaim.setInsuranceID(results.getString("insuranceID"));
@@ -53,23 +64,19 @@ public class CompensationClaimDao extends Dao{
 			compensationClaim.setAccountNumber(results.getString("accountNumber"));
 			compensationClaim.setAccountHolderName(results.getString("accountHolderName"));
 			compensationClaimList.add(compensationClaim);
-        }  
+		}
 		return compensationClaimList;
 	}
-
 	public void updateById(String CCID, String column, String content) throws Exception {
-		String query = "UPDATE CompensationClaim SET"+column+"="+content+"WHERE CCID="+CCID+";";
-		System.out.println(query);
+		String query = "UPDATE CompensationClaim SET " + column + "='" + content + "' WHERE CCID='" + CCID + "';";
 		super.update(query);
 	}
 	public void deleteById(String CCID) throws Exception {
-		String query = "DELETE FROM CompensationClaim WHERE CCID="+CCID+";";
-		System.out.println(query);
+		String query = "DELETE FROM CompensationClaim WHERE CCID='" + CCID + "';";
 		super.delete(query);
 	}
 	public void deleteAll() throws Exception {
 		String query = "DELETE FROM CompensationClaim;";
-		System.out.println(query);
 		super.delete(query);
 	}
 }
--- src/Dao/SurveyDao.java
+++ src/Dao/SurveyDao.java
@@ -1,6 +1,7 @@
 package Dao;
 
 import CompensationClaim.Survey;
+import Insurance.Insurance;
 
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -15,30 +16,37 @@ public class SurveyDao extends Dao{
 		}
 	}
 	public void create(Survey survey) throws Exception {
-		String query = "insert into Survey(CCID, managerName, reportFilePath, surveyFee, decisionMoney, responsibility, responsibilityReason) values ('"+
-				survey.getCCID() + "','" + survey.getManagerName() + "','" + survey.getReportFilePath() + "','" + survey.getSurveyFee() + "','"
-				+ survey.getDecisionMoney() + "','" + survey.isResponsibility() + "','" + survey.getResponsibilityReason() + ");";
-		System.out.println(query);
+		String query = "INSERT INTO Survey (CCID, managerName, reportFilePath, surveyFee, decisionMoney, responsibility, responsibilityReason) VALUES ('" +
+				survey.getCCID() + "','" +
+				survey.getManagerName() + "','" +
+				survey.getReportFilePath() + "','" +
+				survey.getSurveyFee() + "','" +
+				survey.getDecisionMoney() + "','" +
+				survey.isResponsibility() + "','" +
+				survey.getResponsibilityReason() + "');";
 		super.create(query);
 	}
 	public void createAll(ArrayList<Survey> surveyList) throws Exception {
 		Survey survey;
 		for(int i=0;i<=surveyList.size();i++) {
 			survey = surveyList.get(i);
-			String query = "insert into Survey(CCID, managerName, reportFilePath, surveyFee, decisionMoney, responsibility, responsibilityReason) values ('"+
-					survey.getCCID() + "','" + survey.getManagerName() + "','" + survey.getReportFilePath() + "','" + survey.getSurveyFee() + "','"
-					+ survey.getDecisionMoney() + "','" + survey.isResponsibility() + "','" + survey.getResponsibilityReason() + ");";
-			System.out.println(query);
-		super.create(query);
+			String query = "INSERT INTO Survey (CCID, managerName, reportFilePath, surveyFee, decisionMoney, responsibility, responsibilityReason) VALUES ('" +
+					survey.getCCID() + "','" +
+					survey.getManagerName() + "','" +
+					survey.getReportFilePath() + "','" +
+					survey.getSurveyFee() + "','" +
+					survey.getDecisionMoney() + "','" +
+					survey.isResponsibility() + "','" +
+					survey.getResponsibilityReason() + "');";
+			super.create(query);
 		}
 	}
 	public ArrayList<Survey> retrieveAll() throws Exception {
 		String query = "select * from Survey;";
-		System.out.println(query);
 		ResultSet results = super.retrieve(query);
-        ArrayList<Survey> surveyList = new ArrayList<Survey>();
+		ArrayList<Survey> surveyList = new ArrayList<Survey>();
 		Survey survey;
-        while (results.next()){
+		while (results.next()){
 			survey = new Survey();
 			survey.setCCID(results.getString("CCID"));
 			survey.setManagerName(results.getString("managerName"));
@@ -48,29 +56,25 @@ public class SurveyDao extends Dao{
 			survey.setResponsibility(results.getBoolean("responsibility"));
 			survey.setResponsibilityReason(results.getString("responsibilityReason"));
 			surveyList.add(survey);
-        }  
+		}
 		return surveyList;
 	}
 	public void update(Survey survey) throws Exception {
-		//쿼리 제조
-		String query =  "UPDATE Survey SET CCID = '"+survey.getCCID()+"', managerName = '"+survey.getManagerName()+"', reportFilePath = "
-				+survey.getReportFilePath()+", surveyFee = '"+survey.getSurveyFee()+"', decisionMoney = '"+survey.getDecisionMoney()+"', "
-				+ "responsibility = '"+survey.isResponsibility()+"', responsibilityReason = '"+survey.getResponsibilityReason()+" WHERE CCID ='" +survey.getCCID()+"';";
+		String query = "UPDATE Survey SET CCID = '" + survey.getCCID() + "', managerName = '" + survey.getManagerName() + "', reportFilePath = '"
+				+ survey.getReportFilePath() + "', surveyFee = '" + survey.getSurveyFee() + "', decisionMoney = '" + survey.getDecisionMoney() + "', "
+				+ "responsibility = '" + survey.isResponsibility() + "', responsibilityReason = '" + survey.getResponsibilityReason() + "' WHERE CCID = '" + survey.getCCID() + "';";
 		super.update(query);
 	}
 	public void updateById(String CCID, String column, String content) throws Exception {
-		String query = "UPDATE Survey SET"+column+"="+content+"WHERE CCID="+CCID+";";
-		System.out.println(query);
+		String query = "UPDATE Survey SET " + column + "='" + content + "' WHERE CCID='" + CCID + "';";
 		super.update(query);
 	}
 	public void deleteById(String CCID) throws Exception {
-		String query = "DELETE FROM Survey WHERE CCID="+CCID+";";
-		System.out.println(query);
+		String query = "DELETE FROM Survey WHERE CCID='" + CCID + "';";
 		super.delete(query);
 	}
 	public void deleteAll() throws Exception {
 		String query = "DELETE FROM Survey;";
-		System.out.println(query);
 		super.delete(query);
 	}
 }
