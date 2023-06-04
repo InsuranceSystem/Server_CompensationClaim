f53f82deaa77e28f4f053930fd50b38d35e979c5
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2023-06-05 07:23:15 +0900

    [Feat] Exception

--- src/CCServer.java
+++ src/CCServer.java
@@ -1,5 +1,4 @@
 
-
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
@@ -15,52 +14,64 @@ import CompensationClaim.CompensationClaimList;
 import CompensationClaim.CompensationClaimListImpl;
 import CompensationClaim.SurveyList;
 import CompensationClaim.SurveyListImpl;
+import Exception.CustomClassNotFoundException;
+import Exception.CustomConnectException;
+import Exception.CustomIllegalAccessException;
+import Exception.CustomNotBoundException;
+import Exception.CustomSQLException;
 import Interface.CC_ServerIF;
 
-
-
-
-public class CCServer extends UnicastRemoteObject implements CC_ServerIF{
+public class CCServer extends UnicastRemoteObject implements CC_ServerIF {
 	private static final long serialVersionUID = 1L;
 	private static CarAccidentList CarAccidentList;
 	private static SurveyList SurveyList;
 	private static CompensationClaimList CompensationClaimList;
-	
+
 	protected CCServer() throws RemoteException {
 		super();
 	}
-	
-	public static void main(String[] args) throws Exception {
+
+	public static void main(String[] args) throws Exception, CustomNotBoundException, CustomConnectException,
+			CustomIllegalAccessException, CustomClassNotFoundException, NoClassDefFoundError {
 		try {
-			
-		//	Naming.rebind("CompensationClaim_Server", server);	
+
+			// Naming.rebind("CompensationClaim_Server", server);
 			Registry registry = LocateRegistry.createRegistry(1300);
 			CCServer server = new CCServer();
-	        registry.rebind("CompensationClaim_Server", server);	
+			registry.rebind("CompensationClaim_Server", server);
 			CarAccidentList = new CarAccidentListImpl();
 			SurveyList = new SurveyListImpl();
 			CompensationClaimList = new CompensationClaimListImpl();
-					
-			System.out.println("CompensationClaim Server is ready !!!");		
-	
+
+			System.out.println("CompensationClaim Server is ready !!!");
+
 		} catch (RemoteException e) {
-			e.printStackTrace();
+			System.out.println("Remote exception occurred: " + e.getMessage());
+		} catch (CustomNotBoundException e) {
+			System.out.println("Not bound exception occurred: " + e.getMessage());
 		} catch (MalformedURLException e) {
-			e.printStackTrace();	
+			System.out.println("MalformedURLException occurred: " + e.getMessage());
+		} catch (CustomConnectException e) {
+			System.out.println("Connection exception occurred:  " + e.getMessage());
+		} catch (IllegalAccessException e) {
+			System.out.println("Illegal access exception occurred: " + e.getMessage());
+		} catch (CustomClassNotFoundException | NoClassDefFoundError e) {
+			System.out.println("Class Found Error: " + e.getMessage());
 		}
 	}
+
 	@Override
-	public CarAccidentList getCarAccidentList() throws RemoteException{
+	public CarAccidentList getCarAccidentList() throws RemoteException {
 		return CarAccidentList;
 	}
 
 	@Override
-	public SurveyList getSurveyList() throws RemoteException{
+	public SurveyList getSurveyList() throws RemoteException {
 		return SurveyList;
 	}
-	
+
 	@Override
-	public CompensationClaimList getCompensationClaimList() throws RemoteException{
+	public CompensationClaimList getCompensationClaimList() throws RemoteException {
 		return CompensationClaimList;
 	}
 }
--- src/Dao/CarAccidentDao.java
+++ src/Dao/CarAccidentDao.java
@@ -1,73 +1,96 @@
 package Dao;
 
 import CompensationClaim.CarAccident;
+import Exception.DaoException;
 
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-public class CarAccidentDao extends Dao{
-	public CarAccidentDao() {
+public class CarAccidentDao extends Dao {
+	public CarAccidentDao() throws DaoException {
+        try {
+            super.connect();
+        } catch (Exception e) {
+            System.out.println("데이터베이스 연결에 실패했습니다." + e.getMessage());
+            System.out.println("DAO Exception 발생한 메서드: " + ((DaoException) e).getDaoMethodName());
+        }
+	}
+
+	public void create(CarAccident carAccident) throws DaoException {
 		try {
-			super.connect();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	public void create(CarAccident carAccident) throws Exception {
-		String query = "INSERT INTO CarAccident (CCID, type, dateTime, carNumber, driverName, licenseNumber, accidentDetail) VALUES ('" +
-				carAccident.getCCID() + "','" +
-				carAccident.getType() + "','" +
-				carAccident.getDateTime() + "','" +
-				carAccident.getCarNumber() + "','" +
-				carAccident.getDriverName() + "','" +
-				carAccident.getLicenseNumber() + "','" +
-				carAccident.getAccidentDetail() + "');";
-		super.create(query);
-	}
-	public void createAll(ArrayList<CarAccident> carAccidentList) throws Exception {
-		CarAccident carAccident;
-		for(int i=0;i<=carAccidentList.size();i++) {
-			carAccident = carAccidentList.get(i);
-			String query = "INSERT INTO CarAccident (CCID, type, dateTime, carNumber, driverName, licenseNumber, accidentDetail) VALUES ('" +
-					carAccident.getCCID() + "','" +
-					carAccident.getType() + "','" +
-					carAccident.getDateTime() + "','" +
-					carAccident.getCarNumber() + "','" +
-					carAccident.getDriverName() + "','" +
-					carAccident.getLicenseNumber() + "','" +
-					carAccident.getAccidentDetail() + "');";
+			String query = "INSERT INTO CarAccident (CCID, type, dateTime, carNumber, driverName, licenseNumber, accidentDetail) VALUES ('"
+					+ carAccident.getCCID() + "','" + carAccident.getType() + "','" + carAccident.getDateTime() + "','"
+					+ carAccident.getCarNumber() + "','" + carAccident.getDriverName() + "','"
+					+ carAccident.getLicenseNumber() + "','" + carAccident.getAccidentDetail() + "');";
 			super.create(query);
+		} catch (Exception e) {
+			throw new DaoException("CarAccident 생성 중에 오류가 발생했습니다.", "create");
 		}
 	}
-	public ArrayList<CarAccident> retrieveAll() throws Exception {
-		String query = "select * from CarAccident;";
-		ResultSet results = super.retrieve(query);
-		ArrayList<CarAccident> carAccidentList = new ArrayList<CarAccident>();
-		CarAccident carAccident;
-		while (results.next()){
-			carAccident = new CarAccident();
-			carAccident.setCCID(results.getString("CCID"));
-			carAccident.setType(results.getString("type"));
-			carAccident.setDateTime(results.getTimestamp("dateTime").toLocalDateTime());
-			carAccident.setCarNumber(results.getString("carNumber"));
-			carAccident.setDriverName(results.getString("driverName"));
-			carAccident.setLicenseNumber(results.getString("licenseNumber"));
-			carAccident.setAccidentDetail(results.getString("accidentDetail"));
-			carAccidentList.add(carAccident);
+
+	public void createAll(ArrayList<CarAccident> carAccidentList) throws DaoException {
+		try {
+			CarAccident carAccident;
+			for (int i = 0; i < carAccidentList.size(); i++) {
+				carAccident = carAccidentList.get(i);
+				String query = "INSERT INTO CarAccident (CCID, type, dateTime, carNumber, driverName, licenseNumber, accidentDetail) VALUES ('"
+						+ carAccident.getCCID() + "','" + carAccident.getType() + "','" + carAccident.getDateTime()
+						+ "','" + carAccident.getCarNumber() + "','" + carAccident.getDriverName() + "','"
+						+ carAccident.getLicenseNumber() + "','" + carAccident.getAccidentDetail() + "');";
+				super.create(query);
+			}
+		} catch (Exception e) {
+			throw new DaoException("CarAccident 생성 중에 오류가 발생했습니다.", "createAll");
 		}
-		return carAccidentList;
 	}
-	public void updateById(String CCID, String column, String content) throws Exception {
-		String query = "UPDATE CarAccident SET " + column + "='" + content + "' WHERE CCID='" + CCID + "';";
-		super.update(query);
+
+	public ArrayList<CarAccident> retrieveAll() throws DaoException {
+		try {
+			String query = "SELECT * FROM CarAccident;";
+			ResultSet results = super.retrieve(query);
+			ArrayList<CarAccident> carAccidentList = new ArrayList<>();
+			CarAccident carAccident;
+			while (results.next()) {
+				carAccident = new CarAccident();
+				carAccident.setCCID(results.getString("CCID"));
+				carAccident.setType(results.getString("type"));
+				carAccident.setDateTime(results.getTimestamp("dateTime").toLocalDateTime());
+				carAccident.setCarNumber(results.getString("carNumber"));
+				carAccident.setDriverName(results.getString("driverName"));
+				carAccident.setLicenseNumber(results.getString("licenseNumber"));
+				carAccident.setAccidentDetail(results.getString("accidentDetail"));
+				carAccidentList.add(carAccident);
+			}
+			return carAccidentList;
+		} catch (Exception e) {
+			throw new DaoException("CarAccident 목록을 가져오는 중에 오류가 발생했습니다.", "retrieveAll");
+		}
 	}
-	public void deleteById(String CCID) throws Exception {
-		String query = "DELETE FROM CarAccident WHERE CCID="+CCID+";";
-		super.delete(query);
+
+	public void updateById(String CCID, String column, String content) throws DaoException {
+		try {
+			String query = "UPDATE CarAccident SET " + column + "='" + content + "' WHERE CCID='" + CCID + "';";
+			super.update(query);
+		} catch (Exception e) {
+			throw new DaoException("CarAccident 업데이트 중에 오류가 발생했습니다.", "updateById");
+		}
 	}
-	public void deleteAll() throws Exception {
-		String query = "DELETE FROM CarAccident;";
-		super.delete(query);
+
+	public void deleteById(String CCID) throws DaoException {
+		try {
+			String query = "DELETE FROM CarAccident WHERE CCID='" + CCID + "';";
+			super.delete(query);
+		} catch (Exception e) {
+			throw new DaoException("CarAccident 삭제 중에 오류가 발생했습니다.", "deleteById");
+		}
+	}
+
+	public void deleteAll() throws DaoException {
+		try {
+			String query = "DELETE FROM CarAccident;";
+			super.delete(query);
+		} catch (Exception e) {
+			throw new DaoException("CarAccident 삭제 중에 오류가 발생했습니다.", "deleteAll");
+		}
 	}
 }
